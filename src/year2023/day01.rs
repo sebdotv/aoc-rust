d20aaf0446e3bf3c1419478e8c1c0f05af6c6d0c
sebdotv <sebdotv@gmail.com>
2023-12-01 12:05:51 +0100

    add unit test

--- src/year2023/day01.rs
+++ src/year2023/day01.rs
@@ -31,31 +31,54 @@ fn part1(data: &str) -> Result<u32> {
     Ok(values.iter().sum())
 }
 
-fn part2(data: &str) -> Result<u32> {
-    let digits = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-    let re = format!("[0-9]|{}", digits.join("|"));
-    let re = Regex::new(re.as_str()).unwrap();
-    let parse = |s: &str| -> Result<u32> {
+struct Part2Extractor {
+    digits: [&'static str; 9],
+    re: Regex,
+}
+impl Part2Extractor {
+    fn new() -> Self {
+        let digits = [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ];
+        let re = format!("[0-9]|{}", digits.join("|"));
+        let re = Regex::new(re.as_str()).unwrap();
+        Self { digits, re }
+    }
+
+    fn process(&self, s: &str) -> Result<(u32, u32)> {
+        let matches = self.re.find_iter(s).map(|m| m.as_str()).collect_vec();
+        let first: u32 = self.parse_digit(matches.first().unwrap())?;
+        let last: u32 = self.parse_digit(matches.last().unwrap())?;
+        println!(
+            "line: {}; matches: {:?}; result: {:?}",
+            s,
+            matches,
+            (first, last)
+        );
+        Ok((first, last))
+    }
+
+    fn parse_digit(&self, s: &str) -> Result<u32> {
         if let Ok(n) = s.parse::<u32>() {
             Ok(n)
         } else {
-            let n = digits
+            let n = self
+                .digits
                 .iter()
                 .position(|&d| d == s)
                 .ok_or(anyhow!("not a spelled digit"))?
                 + 1;
             Ok(n as u32)
         }
-    };
+    }
+}
+
+fn part2(data: &str) -> Result<u32> {
+    let extractor = Part2Extractor::new();
     let values = data
         .lines()
         .map(|line| {
-            let matches = re.find_iter(line).map(|m| m.as_str()).collect_vec();
-            // println!("line: {}; matches: {:?}", line, matches);
-            let first: u32 = parse(matches.first().unwrap()).unwrap();
-            let last: u32 = parse(matches.last().unwrap()).unwrap();
+            let (first, last) = extractor.process(line)?;
             Ok(first * 10 + last)
         })
         .collect::<Result<Vec<_>>>()?;
@@ -69,4 +92,16 @@ where
     let char = chars.find(|c| c.is_ascii_digit())?;
     let digit = char.to_digit(10).expect("unexpected");
     Some(digit)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part2_extractor_works() {
+        let data = "6twodndmhcgxlgbqbqndbbthnngblfgtzh5fouroneightrjp";
+        let (first, last) = Part2Extractor::new().process(data).unwrap();
+        assert_eq!((first, last), (6, 8));
+    }
 }
