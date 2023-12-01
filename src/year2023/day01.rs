8f5862d75596ab5b16fe2907f8b3525e9658c798
sebdotv <sebdotv@gmail.com>
2023-12-01 10:25:54 +0100

    p1

--- src/year2023/day01.rs
+++ src/year2023/day01.rs
@@ -1,10 +1,10 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
 use crate::challenge::ChallengeDay;
 
-pub fn day() -> ChallengeDay<i32> {
+pub fn day() -> ChallengeDay<u32> {
     ChallengeDay {
-        part1_solutions: (142, None),
+        part1_solutions: (142, Some(54390)),
         part2_solutions: None,
         part1_solver: part1,
         part2_solver: part2,
@@ -12,10 +12,31 @@ pub fn day() -> ChallengeDay<i32> {
     }
 }
 
-fn part1(data: &str) -> Result<i32> {
+fn part1(data: &str) -> Result<u32> {
+    let values = data
+        .lines()
+        .map(|line| {
+            let chars = line.chars();
+            let chars_rev = line.chars().rev();
+            let (first, last) = (
+                first_digit(chars).ok_or(anyhow!("no first digit"))?,
+                first_digit(chars_rev).ok_or(anyhow!("no last digit"))?,
+            );
+            Ok(first * 10 + last)
+        })
+        .collect::<Result<Vec<_>>>()?;
+    Ok(values.iter().sum())
+}
+
+fn part2(_data: &str) -> Result<u32> {
     todo!()
 }
 
-fn part2(_data: &str) -> Result<i32> {
-    todo!()
+fn first_digit<I>(mut chars: I) -> Option<u32>
+where
+    I: Iterator<Item = char>,
+{
+    let char = chars.find(|c| c.is_ascii_digit())?;
+    let digit = char.to_digit(10).expect("unexpected");
+    Some(digit)
 }
