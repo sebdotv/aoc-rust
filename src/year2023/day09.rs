3d07403a3f5a661f662538a62bc75f7f9db5fb3c
sebdotv <sebdotv@gmail.com>
2023-12-10 16:56:34 +0100

    cleanup

--- src/year2023/day09.rs
+++ src/year2023/day09.rs
@@ -1,4 +1,4 @@
-use anyhow::{Error, Result};
+use anyhow::{anyhow, Context, Result};
 use itertools::Itertools;
 
 use crate::challenge::Day;
@@ -6,7 +6,7 @@ use crate::challenge::Day;
 pub fn day() -> Day<i32> {
     Day {
         part1_solutions: (114, Some(1731106378)),
-        part2_solutions: Some((2, None)),
+        part2_solutions: Some((2, Some(1087))),
         part1_solver: part1,
         part2_solver: part2,
         source_file: file!(),
@@ -22,14 +22,16 @@ fn part2(data: &str) -> Result<i32> {
     sum_of_next_values(data, &Side::Left)
 }
 
-fn sum_of_next_values(data: &str, side: &Side) -> Result<i32, Error> {
+fn sum_of_next_values(data: &str, side: &Side) -> Result<i32> {
     let sum = data
         .lines()
         .map(|line| {
             line.split_ascii_whitespace()
-                .map(|s| s.parse::<i32>().unwrap())
-                .collect_vec()
+                .map(|s| s.parse::<i32>().with_context(|| anyhow!("parse error")))
+                .collect::<Result<Vec<_>>>()
         })
+        .collect::<Result<Vec<_>>>()?
+        .iter()
         .map(|values| compute_next_value(values, side))
         .sum();
     Ok(sum)
@@ -40,7 +42,7 @@ enum Side {
     Left,
 }
 
-fn compute_next_value(values: Vec<i32>, side: &Side) -> i32 {
+fn compute_next_value(values: &[i32], side: &Side) -> i32 {
     if values.iter().all(|&v| v == 0) {
         0
     } else {
@@ -49,7 +51,7 @@ fn compute_next_value(values: Vec<i32>, side: &Side) -> i32 {
             .zip(values.iter().skip(1))
             .map(|(a, b)| b - a)
             .collect_vec();
-        let diff = compute_next_value(next_layer, side);
+        let diff = compute_next_value(&next_layer, side);
         match side {
             Side::Right => values.last().unwrap() + diff,
             Side::Left => values.first().unwrap() - diff,
