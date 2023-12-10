17efd5c7acb4b38099e6ac30f0f33b83d3fa2428
sebdotv <sebdotv@gmail.com>
2023-12-10 16:17:28 +0100

    wip

--- src/year2022/day07.rs
+++ src/year2022/day07.rs
@@ -1,7 +1,9 @@
 use std::iter;
+use std::ops::Index;
 use std::str::FromStr;
 
 use anyhow::{anyhow, Result};
+use indexmap::map::Entry;
 use indexmap::IndexMap;
 use itertools::Itertools;
 
@@ -30,86 +32,153 @@ fn part1(data: &str) -> Result<u32> {
                     .join("\n")
             })
         })
-        .map(|s| s.parse::<Command>().unwrap())
+        .map(|s| s.parse::<Command>())
         .inspect(|cmd| {
             println!("{:?}", cmd);
             println!();
         })
-        .collect_vec();
+        .collect::<Result<Vec<_>>>()?;
 
     compute_dir_sizes(commands);
 
     Ok(0)
 }
 
+// fn compute_dir_sizes_2(commands: Vec<Command>) -> Result<u32> {
+//
+//
+//
+//     Ok(0)
+// }
+
 fn compute_dir_sizes(commands: Vec<Command>) -> Result<u32> {
     type Path = Vec<String>;
     struct DirIndex {
+        curr_path: Path,
         dirs: IndexMap<Path, Dir>,
+    }
+    impl DirIndex {
+        pub fn process(&mut self, cmd: Command) {
+            use CdCommand::*;
+            use Command::*;
+            match cmd {
+                Cd(cd_cmd) => {
+                    self.curr_path = match cd_cmd {
+                        CdSlash => vec![],
+                        CdUp => Dir::parent_path(&self.curr_path),
+                        CdDir(ref name) => Dir::make_path(&self.curr_path, name),
+                    }
+                }
+                Ls(ls_cmd) => {
+                    let dir = Dir {
+                        entries: ls_cmd.entries,
+                    };
+                    let prev = self.dirs.insert(self.curr_path.clone(), dir);
+                    assert!(prev.is_none());
+                }
+            }
+        }
     }
     impl DirIndex {
         // fn insert(&mut self, dir: Dir) {
         //     let prev = self.dirs.insert(dir.path(), dir);
         //     assert!(prev.is_none());
         // }
-        fn get(&self, path: &Path) -> &Dir {
-            self.dirs.get(path).unwrap()
-        }
-        fn get_or_create(&mut self, parent_path: &Path, name: &String) -> &Dir {
-            self.dirs
-                .entry(Dir::make_path(parent_path, name))
-                .or_insert_with(|| Dir {
-                    name: name.clone(),
-                    entries: vec![],
-                    parent_path: parent_path.clone(),
-                })
-        }
+        // fn get(&self, path: &Path) -> &Dir {
+        //     self.dirs.get(path).unwrap()
+        // }
+        // fn get_or_create(&mut self, parent_path: &Path, name: &String) -> &Dir {
+        //     self.dirs
+        //         .entry(Dir::make_path(parent_path, name))
+        //         .or_insert_with(|| Dir {
+        //             name: name.clone(),
+        //             entries: vec![],
+        //             parent_path: parent_path.clone(),
+        //         })
+        // }
     }
     struct Dir {
-        name: String,
-        entries: Vec<DirEntry>,
-        parent_path: Path,
+        // name: String,
+        entries: Vec<LsEntry>,
+        // parent_path: Path,
     }
     impl Dir {
-        fn path(&self) -> Path {
-            Self::make_path(&self.parent_path, &self.name)
-        }
+        // fn path(&self) -> Path {
+        //     Self::make_path(&self.parent_path, &self.name)
+        // }
         fn make_path(parent_path: &Vec<String>, name: &String) -> Path {
             let mut path = parent_path.clone();
             path.push(name.clone());
             path
         }
+        fn parent_path(path: &Path) -> Path {
+            let mut path = path.clone();
+            path.pop().unwrap();
+            path
+        }
     }
-    enum DirEntry {
-        Dir(Dir),
-        File(u32, String),
-    }
+    // enum DirEntry {
+    //     Dir(Dir),
+    //     File(u32, String),
+    // }
     let mut index = DirIndex {
+        curr_path: vec![],
         dirs: IndexMap::new(),
     };
+
+    for cmd in commands {
+        index.process(cmd);
+    }
+
+    struct DirWalker {
+        index: DirIndex,
+        total_sizes: IndexMap<Path, u32>,
+    }
+    impl DirWalker {
+        fn get_or_compute_total_size(&mut self, path: &Path) -> u32 {
+            let entry = self.total_sizes.entry(path.clone());
+            match entry {
+                Entry::Occupied(occupied) => occupied.get().clone(),
+                Entry::Vacant(vacant) => {
+                    let dir = self.index.dirs.get(path).unwrap();
+                    let total_size = dir.entries.iter().map(self.visit).sum();
+                    vacant.insert(total_size);
+                    total_size
+                }
+            }
+        }
+
+        fn visit() {
+            |entry| match entry {
+                LsEntry::File(size, _) => size.clone(),
+                LsEntry::Dir(name) => self.get_or_compute_total_size(&Dir::make_path(path, name)),
+            }
+        }
+    }
+
     // let root = index.get_or_create(&vec![], &"".to_owned());
     // let mut current_dir = index.get(&Vec::new());
 
-    let root: Path = vec![];
-    let mut current_path: Path = root.clone();
-
-    for cmd in commands {
-        match cmd {
-            Command::Cd(cd_cmd) => {
-                current_path = match cd_cmd {
-                    CdCommand::CdSlash => root.clone(),
-                    CdCommand::CdUp => current_path.iter().dropping_back(1).cloned().collect(),
-                    CdCommand::CdDir(dir_name) => current_path
-                        .iter()
-                        .chain(iter::once(&dir_name))
-                        .cloned()
-                        .collect(),
-                };
-                println!("{:?}", current_path);
-            }
-            Command::Ls(LsCommand { entries }) => todo!(),
-        }
-    }
+    // let root: Path = vec![];
+    // let mut current_path: Path = root.clone();
+    //
+    // for cmd in commands {
+    //     match cmd {
+    //         Command::Cd(cd_cmd) => {
+    //             current_path = match cd_cmd {
+    //                 CdCommand::CdSlash => root.clone(),
+    //                 CdCommand::CdUp => current_path.iter().dropping_back(1).cloned().collect(),
+    //                 CdCommand::CdDir(dir_name) => current_path
+    //                     .iter()
+    //                     .chain(iter::once(&dir_name))
+    //                     .cloned()
+    //                     .collect(),
+    //             };
+    //             println!("{:?}", current_path);
+    //         }
+    //         Command::Ls(LsCommand { entries }) => todo!(),
+    //     }
+    // }
 
     // let mut current_dir: Vec<&str> = vec![];
     // let mut dir_contents: IndexMap<Vec<&str>, Vec> = 0;
