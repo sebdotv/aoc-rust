1e32cb1c9c8355b89b8d229ae5654f6a6c0de3ce
sebdotv <sebdotv@gmail.com>
2023-12-22 08:47:32 +0100

    22p1 wip

--- src/year2023/day22.rs
+++ src/year2023/day22.rs
@@ -1,4 +1,7 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
+use itertools::Itertools;
+use std::ops::RangeInclusive;
+use std::str::FromStr;
 
 use crate::challenge::Day;
 
@@ -14,9 +17,82 @@ pub fn day() -> Day<usize> {
 }
 
 fn part1(data: &str) -> Result<usize> {
-    todo!()
+    let bricks = data
+        .lines()
+        .map(Brick::from_str)
+        .collect::<Result<Vec<_>>>()?;
+    for brick in &bricks {
+        println!("{:?}", brick);
+    }
+    Ok(5)
 }
 
 fn part2(_data: &str) -> Result<usize> {
     todo!()
+}
+
+#[derive(Debug)]
+enum Brick {
+    Cube {
+        x: usize,
+        y: usize,
+        z: usize,
+    },
+    XLine {
+        x: RangeInclusive<usize>,
+        y: usize,
+        z: usize,
+    },
+    YLine {
+        x: usize,
+        y: RangeInclusive<usize>,
+        z: usize,
+    },
+    ZLine {
+        x: usize,
+        y: usize,
+        z: RangeInclusive<usize>,
+    },
+}
+impl FromStr for Brick {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (start, end) = s.split_once('~').unwrap();
+        let parse_xyz = |s: &str| {
+            let (x, y, z) = s
+                .split(',')
+                .map(|s| s.parse::<usize>().unwrap())
+                .collect_tuple()
+                .unwrap();
+            (x, y, z)
+        };
+        let (start_x, start_y, start_z) = parse_xyz(start);
+        let (end_x, end_y, end_z) = parse_xyz(end);
+        let (len_x, len_y, len_z) = (end_x - start_x, end_y - start_y, end_z - start_z);
+        let brick = match (len_x, len_y, len_z) {
+            (0, 0, 0) => Brick::Cube {
+                x: start_x,
+                y: start_y,
+                z: start_z,
+            },
+            (x, 0, 0) if x > 0 => Brick::XLine {
+                x: start_x..=end_x,
+                y: start_y,
+                z: start_z,
+            },
+            (0, y, 0) if y > 0 => Brick::YLine {
+                x: start_x,
+                y: start_y..=end_y,
+                z: start_z,
+            },
+            (0, 0, z) if z > 0 => Brick::ZLine {
+                x: start_x,
+                y: start_y,
+                z: start_z..=end_z,
+            },
+            _ => bail!("Invalid brick: {}", s),
+        };
+        Ok(brick)
+    }
 }
