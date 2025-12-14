d6c43c1953b498fb071ad8382362faffd5918cf0
sebdotv <sebdotv@gmail.com>
2025-12-14 22:50:59 +0100

    add 04

--- src/year2025/day04.rs
+++ src/year2025/day04.rs
@@ -1,11 +1,13 @@
-use anyhow::Result;
-
 use crate::challenge::Day;
+use crate::utils::grid::{Coord, Grid};
+use anyhow::Result;
+use itertools::Itertools;
+use strum_macros::EnumString;
 
 pub fn day() -> Day<usize> {
     Day {
-        part1_solutions: (todo!(), None),
-        part2_solutions: None,
+        part1_solutions: (13, Some(1397)),
+        part2_solutions: Some((43, None)),
         part1_solver: part1,
         part2_solver: part2,
         source_file: file!(),
@@ -13,10 +15,49 @@ pub fn day() -> Day<usize> {
     }
 }
 
-fn part1(data: &str) -> Result<usize> {
-    todo!()
+#[derive(EnumString, Eq, PartialEq)]
+enum Cell {
+    #[strum(serialize = ".")]
+    Empty,
+    #[strum(serialize = "@")]
+    Roll,
 }
 
-fn part2(_data: &str) -> Result<usize> {
-    todo!()
+fn part1(data: &str) -> Result<usize> {
+    let grid: Grid<Cell> = data.parse().unwrap();
+    Ok(find_accessible(&grid).len())
+}
+
+fn find_accessible(grid: &Grid<Cell>) -> Vec<Coord> {
+    grid.coords()
+        .filter(|c| grid.get(c) == &Cell::Roll)
+        .filter(|c| {
+            let rolls = grid
+                .neighbors_incl_diag(c)
+                .iter()
+                .filter(|c| grid.get(c) == &Cell::Roll)
+                .count();
+            rolls < 4
+        })
+        .collect_vec()
+}
+
+fn part2(data: &str) -> Result<usize> {
+    let mut grid: Grid<Cell> = data.parse().unwrap();
+
+    let mut removed = 0;
+    loop {
+        let accessible = find_accessible(&grid);
+        if accessible.is_empty() {
+            break;
+        }
+
+        for c in &accessible {
+            grid.set(c, Cell::Empty);
+        }
+
+        removed += accessible.len();
+    }
+
+    Ok(removed)
 }
--- src/year2025/mod.rs
+++ src/year2025/mod.rs
@@ -3,7 +3,7 @@ use crate::challenge::DayWrapper;
 // mod day01;
 // mod day02;
 // mod day03;
-// mod day04;
+mod day04;
 mod day05;
 mod day06;
 mod day07;
@@ -19,7 +19,7 @@ pub fn challenge_days() -> Vec<DayWrapper> {
         // day01::day().into(),
         // day02::day().into(),
         // day03::day().into(),
-        // day04::day().into(),
+        day04::day().into(),
         day05::day().into(),
         day06::day().into(),
         day07::day().into(),
