5acc37fb5f48f6d14d0b9117977711a7b05964ef
sebdotv <sebdotv@gmail.com>
2025-12-19 17:32:18 +0100

    02

--- src/year2025/day02.rs
+++ src/year2025/day02.rs
@@ -1,11 +1,11 @@
-use anyhow::Result;
-
 use crate::challenge::Day;
+use anyhow::Result;
+use itertools::Itertools;
 
 pub fn day() -> Day<usize> {
     Day {
-        part1_solutions: (todo!(), None),
-        part2_solutions: None,
+        part1_solutions: (1227775554, Some(30599400849)),
+        part2_solutions: Some((4174379265, Some(46270373595))),
         part1_solver: part1,
         part2_solver: part2,
         source_file: file!(),
@@ -14,9 +14,40 @@ pub fn day() -> Day<usize> {
 }
 
 fn part1(data: &str) -> Result<usize> {
-    todo!()
+    sum_of_invalid(data, is_invalid_part1)
+}
+fn part2(data: &str) -> Result<usize> {
+    sum_of_invalid(data, is_invalid_part2)
 }
 
-fn part2(_data: &str) -> Result<usize> {
-    todo!()
+fn is_invalid_part1(i: usize) -> bool {
+    let s = i.to_string();
+    let (a, b) = s.split_at(s.len() / 2);
+    a == b
+}
+fn is_invalid_part2(i: usize) -> bool {
+    let s = i.to_string();
+    let chars = s.chars().collect_vec();
+    for n in 1..s.len() {
+        let mut it = chars.chunks(n);
+        let first = it.next().unwrap();
+        if it.all(|chunk| chunk == first) {
+            return true;
+        }
+    }
+    false
+}
+
+fn sum_of_invalid(data: &str, f: impl Fn(usize) -> bool) -> Result<usize> {
+    Ok(data
+        .trim_end()
+        .split(',')
+        .flat_map(|s| {
+            let (first, last) = s.split_once('-').unwrap();
+            let first: usize = first.parse().unwrap();
+            let last: usize = last.parse().unwrap();
+            first..=last
+        })
+        .filter(|i| f(*i))
+        .sum())
 }
--- src/year2025/mod.rs
+++ src/year2025/mod.rs
@@ -1,7 +1,7 @@
 use crate::challenge::DayWrapper;
 
 // mod day01;
-// mod day02;
+mod day02;
 mod day03;
 mod day04;
 mod day05;
@@ -17,7 +17,7 @@ mod day12;
 pub fn challenge_days() -> Vec<DayWrapper> {
     vec![
         // day01::day().into(),
-        // day02::day().into(),
+        day02::day().into(),
         day03::day().into(),
         day04::day().into(),
         day05::day().into(),
